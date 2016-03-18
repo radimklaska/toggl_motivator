70aebc5c6302f8d9ff46f34f092b894dd7aaf59f
Radim Klaska <radim@klaska.net>
2016-03-18 17:00:15 +0100

    Added few hints in config file.

--- config.example.php
+++ config.example.php
@@ -1,12 +1,17 @@
 <?php
 
+// Create copy of this file named config.php and edit all values there.
+
 // Toggl API.
+// @See: https://github.com/toggl/toggl_api_docs/blob/master/reports.md#request-parameters
 define("TOGGL_AGENT", '');
 define("TOGGL_TOKEN", '');
 define("TOGGL_WORKSPACE_ID", '');
 define("TOGGL_USER_ID", '');
 
-// Info.
+// How much do you make per hour?
 define("HOUR_RATE", 1);
+// In which currency?
 define("HOUR_RATE_CURRENCY", 'CZK');
+// Results will use this currency:
 define("FINAL_CURRENCY", 'CZK');
